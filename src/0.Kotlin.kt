d8224dd4d0ec26523fbf615a5441fc8517fa91c4
SeungHyunBaek <back947@naver.com>
2020-12-23 16:54:10 +0900

    Create Kotlin Project / 코틀린 프로젝트 생성

---
+++ src/0.Kotlin.kt
@@ -0,0 +1,9 @@
+data class Person(val name: String, val age: Int? = null)
+
+fun main(args: Array<String>) {
+    val persons = listOf(Person("영희"), Person("철수", age=29))
+
+    val oldest = persons.maxBy { it.age ?: 0 }
+
+    println("가장 나이가 많은 사람 : $oldest")
+}
