2daafa073797b389e3d4bf3ec0cebb54aafb0796
SeungHyunBaek <back947@naver.com>
2021-01-11 23:48:17 +0900

    5장 람다로 프로그래밍 (람다)

---
+++ src/step5/0.Lambda.kt
@@ -0,0 +1,36 @@
+package scratch.step5
+
+data class Person(val name: String, val age: Int)
+
+fun findTheOldest(people: List<Person>) {
+    var maxAge = 0
+    var theOldest: Person? = null
+
+    for (person in people) {
+        if(person.age > maxAge) {
+            maxAge = person.age
+            theOldest = person
+        }
+    }
+    println(theOldest)
+}
+
+fun main() {
+    val people = listOf(Person("Alice", 29), Person("Bob", 31))
+    findTheOldest(people)
+
+    println(people.maxBy { it.age })
+    println(people.maxBy(Person::age))
+    println(people.maxBy() {p:Person -> p.age })
+    println(people.maxBy {p: Person -> p.age})
+
+    var names = people.joinToString(separator=" ",
+            transform = {p: Person -> p.name})
+    println(names)
+
+    names = people.joinToString(" ") {p: Person -> p.name}
+    println(names)
+
+    val getAge = {p: Person -> p.age}
+    println(people.maxBy(getAge))
+}
